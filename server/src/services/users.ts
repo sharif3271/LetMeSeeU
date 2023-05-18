a33e1e96503cfc0d011cc979c1910cbe09645eac
sharif eiri <sharif3271@gmail.com>
2023-05-19 02:19:51 +0300

    user-entry logic

--- server/src/services/users.ts
+++ server/src/services/users.ts
@@ -1,44 +1,49 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { IAuthUser, ICompany, IUser, UserType } from "src/interfaces";
+import { IAuthUser, ILoginBody, IRegisterBody } from "src/interfaces";
 import { UserRepository } from "src/repository";
 import { JwtService } from '@nestjs/jwt';
+import { ErrorMessages, IResponse, PassUtils, Successful } from "src/utils";
+import { User } from "@prisma/client";
 
 @Injectable()
 export class UsersService {
-    constructor(private userRepo: UserRepository, private jwtService: JwtService) {}
+  constructor(private repository: UserRepository, private jwtService: JwtService) { }
 
-    private generateAuthToken(type: UserType, data: IUser | ICompany) {
-        return {
-            chatToken: this.jwtService.sign({
-                type,
-                data
-            } as IAuthUser)
-        };
+  private generateToken(user: IAuthUser) {
+    return this.jwtService.sign(user);
+  }
+  async login(body: ILoginBody) {
+    if (!body.name || !body.password) {
+      throw new HttpException(
+        ErrorMessages.user.REGISTER_BAD_REQUEST,
+        HttpStatus.BAD_REQUEST
+      );
     }
-    async getUserProfile(token: string, userType: UserType) {
-        if (!token) {
-            throw new HttpException('User Authentication Faild', HttpStatus.UNAUTHORIZED);
-        }
-        if (userType === UserType.USER) {
-            const user = await this.userRepo.users.findFirst({
-                where: {express_token: token},
-            });
-            if (user) {
-                return this.generateAuthToken(UserType.USER, user);
-            } else {
-                throw new HttpException('Client Not Fount', HttpStatus.NOT_FOUND);
-            }
-        } else if (userType === UserType.COMPANY) {
-            const company = await this.userRepo.companies.findFirst({
-                where: {express_token: token},
-            });
-            if (company) {
-                return this.generateAuthToken(UserType.COMPANY, company);
-            } else {
-                throw new HttpException('Client Not Fount', HttpStatus.NOT_FOUND);
-            }
-        } else {
-            throw new HttpException('User Type Undefined', HttpStatus.BAD_REQUEST);
-        }
+    const user = await this.repository.user.findUnique({ where: { name: body.name } });
+    if (user && PassUtils.verify(user.password, body.password)) {
+      return new Successful({ data: this.generateToken({ id: user.id, name: user.name, })})
+    } else {
+      throw new HttpException(ErrorMessages.user.LOGIN_INVALID, HttpStatus.FORBIDDEN);
     }
+  }
+  async register(body: IRegisterBody) {
+    if (!body.name || !body.password) {
+      throw new HttpException(
+        ErrorMessages.user.REGISTER_BAD_REQUEST,
+        HttpStatus.BAD_REQUEST
+      );
+    }
+    return await this.repository.user.create({
+      data: {
+        name: body.name,
+        password: await PassUtils.makeHash(body.password),
+      }
+    })
+      .then(u => {
+        return new Successful<User>({ data: { ...u, password: undefined } }).toJson();
+      })
+      .catch(() => {
+        throw new HttpException(ErrorMessages.user.ALREADY_EXISTS, HttpStatus.FORBIDDEN);
+      })
+  }
 }
