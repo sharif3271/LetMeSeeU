b911afb8c580922a982affacd71b39bf2bf5ecf8
sharif eiri <sharif3271@gmail.com>
2023-05-19 02:19:28 +0300

    request failiur constant messages

---
+++ server/src/utils/consts/error-messages.ts
@@ -0,0 +1,7 @@
+export const ErrorMessages = {
+  user: {
+    ALREADY_EXISTS: 'User already exists, Please try another name.',
+    REGISTER_BAD_REQUEST: 'Missing values for user name or pawwsord.',
+    LOGIN_INVALID: 'Your entered username or password in incorrect',
+  }
+};
---
+++ server/src/utils/index.ts
@@ -0,0 +1,3 @@
+export * from './consts';
+export * from './response-templates';
+export * from './password';
