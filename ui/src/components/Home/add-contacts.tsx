0da22818d541244d893d8ddcfbbda849553f7d3e
sharif eiri <sharif3271@gmail.com>
2023-06-03 01:06:37 +0300

    add contact component

---
+++ ui/src/components/Home/add-contacts.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { ModalableComponentProps } from 'src/components/shared';
+
+export function AddContact({ close }: ModalableComponentProps) {
+  return <div className='add-contact-container'></div>;
+}
