83117862251a3a087b447c986d797a7dece1763b
sharif eiri <sharif3271@gmail.com>
2023-06-03 01:08:38 +0300

    impliment exit effect programatically

--- ui/src/components/shared/hooks/useAsModal.tsx
+++ ui/src/components/shared/hooks/useAsModal.tsx
@@ -1,10 +1,10 @@
-import React, { FC, useCallback, useState } from 'react';
+import React, { FC, useCallback, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion, useAnimate } from 'framer-motion';
 import { useMemo } from 'react';
 import { FadeInOutVertically } from 'src/components/shared';
 
-export interface ModalableComponentProps<T> {
+export interface ModalableComponentProps<T = unknown> {
   close: (cbData: T) => void;
 }
 
@@ -22,31 +22,44 @@ export function useAsModal<T = unknown>({
 
   const target = useMemo(() => document.getElementById('modal-root') as HTMLElement, []);
 
+  const [exit, setExit] = useState(false);
+  const [scope, animate] = useAnimate();
   const [_show, _setShow] = useState(false);
   const show = useCallback(() => _setShow(true), [_setShow]);
-  const close = useCallback(() => _setShow(false), [_setShow]);
+  const close = useCallback(() => setExit(true), [setExit]);
+
+  useEffect(() => {
+    if (exit) {
+      const exitAnimation = async () => {
+        await animate(scope.current, { opacity: 0.2, y: '100vh' }, { duration: 0.3, ease: 'easeOut' });
+        _setShow(false);
+        setExit(false);
+      };
+      exitAnimation();
+    }
+  }, [exit, _setShow, setExit]);
+
   const closeWithCallback = useCallback((data: unknown) => {
-    _setShow(false);
+    close();
     callback?.(data as any);
-  }, [_setShow]);
-  const Modal = useMemo(() => ReactDOM.createPortal((
-    <AnimatePresence>
-      {
-        _show && (
-          <>
-            <div className={`modal-back-drop ${hasBackDrop && 'bg-[#0000007c]'}`} onClick={close}></div>
-            <motion.div
-              variants={FadeInOutVertically}
-              initial={'initial'}
-              animate={'final'}
-              exit={'exit'}
-            >
-              <RenderComponent close={closeWithCallback}/>
-            </motion.div>
-          </>
-        )
-      }
-    </AnimatePresence>
+  }, []);
+
+  const Modal = useMemo(() => () => ReactDOM.createPortal((
+    _show ? (
+      <>
+        <div className={`modal-back-drop ${hasBackDrop && 'bg-[#0000007c]'}`}></div>
+        <motion.div
+          variants={FadeInOutVertically}
+          initial={'initial'}
+          animate={'final'}
+          onClick={close}
+          ref={scope}
+          className='absolute inset-0 max-w-app mx-auto flex min-h-screen z-50 justify-center items-end md:items-center bg-transparent'
+        >
+          <RenderComponent close={closeWithCallback} />
+        </motion.div>
+      </>
+    ) : null
   ), target), [target, _show]);
 
   return { show, close, Modal };
