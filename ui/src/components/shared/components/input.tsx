bc67da67332e5a8c0e772ce1c104ff58ae684f68
sharif eiri <sharif3271@gmail.com>
2023-06-07 01:31:03 +0300

    add forwarded ref

--- ui/src/components/shared/components/input.tsx
+++ ui/src/components/shared/components/input.tsx
@@ -1,19 +1,22 @@
-import React, { InputHTMLAttributes, useCallback, useState } from 'react';
+import React, { InputHTMLAttributes, forwardRef, useCallback, useState } from 'react';
 import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
 
 interface IAppInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   showPassDefault?: boolean;
+  containerClassName?: string;
 }
 
-export function AppInput({ showPassDefault = false, label, className, ...props }: IAppInputProps) {
+export const AppInput = forwardRef<HTMLInputElement, IAppInputProps>(function AppInputWithRef(InProps: IAppInputProps, ref) {
+  const { showPassDefault = false, label, containerClassName, className, ...props } = InProps;
   const [isShow, setIsShow] = useState(!!showPassDefault);
   const togglePassword = useCallback(() => setIsShow(!isShow), [isShow, setIsShow]);
   return (
-    <label className='input-label-container'>
+    <label className={`${containerClassName || ''} input-label-container`}>
       <p className='input-label'>{label}</p>
       <input
-        className={`app-input ${className || ''}`}
+        className={`${className || ''} app-input`}
+        ref={ref}
         {...{
           ...props,
           type: (props.type !== 'password' ? props.type : (isShow ? 'text' : 'password')),
@@ -28,4 +31,4 @@ export function AppInput({ showPassDefault = false, label, className, ...props }
       }
     </label>
   );
-}
+});
